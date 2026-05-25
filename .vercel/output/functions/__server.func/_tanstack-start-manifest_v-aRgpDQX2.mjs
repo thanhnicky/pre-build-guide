936ce47ae06db63269be0feddbf43c5ee6bfc85c
thanhnicky <suacanho@gmail.com>
2026-05-25 11:57:23 +0700

    update phun-lau2

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-aRgpDQX2.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/macbook/Documents/pre-build-guide/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CDRQcGYu.js"] }, "/": { filePath: "/Users/macbook/Documents/pre-build-guide/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-ijM2y-W-.js"] } }, clientEntry: "/assets/index-CDRQcGYu.js" });
-export {
-  tsrStartManifest
-};

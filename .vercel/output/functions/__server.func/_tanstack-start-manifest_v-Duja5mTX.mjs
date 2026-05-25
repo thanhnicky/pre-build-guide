dd253519f12350a1ab3678f9c9b39138743da5a1
thanhnicky <suacanho@gmail.com>
2026-05-25 10:50:38 +0700

    remove vercel cache

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Duja5mTX.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/macbook/Documents/pre-build-guide/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-JAQ2ed7i.js"] }, "/": { filePath: "/Users/macbook/Documents/pre-build-guide/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-cRbAhSFH.js"] } }, clientEntry: "/assets/index-JAQ2ed7i.js" });
-export {
-  tsrStartManifest
-};

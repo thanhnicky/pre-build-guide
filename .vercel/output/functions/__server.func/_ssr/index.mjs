7a394d694ddfc9d39e69b7b83df3b59bbf221a64
thanhnicky <suacanho@gmail.com>
2026-05-27 23:07:47 +0700

    fix: rebuild with fzoqzuni project id, sync all vercel output

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-BFe0ZmLR.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-sFSMoZrs.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }

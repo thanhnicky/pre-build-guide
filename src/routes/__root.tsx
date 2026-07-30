bdcb3c73f2505e8a332731a48be2e30afe51d9a6
thanhnicky <aicathanh@gmail.com>
2026-07-30 11:19:32 +0700

    Fix GTM - use useEffect to inject script dynamically

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,6 +8,7 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 import appCss from "../styles.css?url";
 
@@ -266,18 +267,17 @@ function RootShell({ children }: { children: React.ReactNode }) {
     }
   ];
 
+  useEffect(() => {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T5PGMZ8D');
+  }, []);
+
   return (
     <html lang="vi">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T5PGMZ8D');`,
-          }}
-        />
         <HeadContent />
         {/* Schema.org JSON-LD */}
         {schemas.map((schema, index) => (

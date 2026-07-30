bf98c543db40f279b36dea5b5cccb97f436e1f62
thanhnicky <aicathanh@gmail.com>
2026-07-30 11:36:30 +0700

    Remove duplicate client-side GTM useEffect, rely solely on server-side HTML injection

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,7 +8,6 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
-import { useEffect } from "react";
 
 import appCss from "../styles.css?url";
 
@@ -266,14 +265,6 @@ function RootShell({ children }: { children: React.ReactNode }) {
       ]
     }
   ];
-
-  useEffect(() => {
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-T5PGMZ8D');
-  }, []);
 
   return (
     <html lang="vi">
